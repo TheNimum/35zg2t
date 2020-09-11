092255f2e21b41a9a460822d9133dbd3621b1aaa
Mikael Pajari <mikael.pajari@live.se>
2020-09-11 13:49:17 +0200

    ej klar

--- Program.cs
+++ Program.cs
@@ -9,26 +9,98 @@ namespace _35zg2t
             month();
             Console.ReadKey ();
         }
-        public static void month()
+        public static void month(string[] args)
             {
-            string [] month =  {"januari", "februari", "mars", "april", "maj", "juni", "juli",
-             "augusti", "september", "oktober", "november", "december"};
-            
-            System.Console.WriteLine("vilken månad är du född i svara med 1-12; ");
-            int dat = int.Parse(Console.ReadLine());
-            var x =  month[dat -1]; 
-            
-            Console.WriteLine($"Du är född i: {x} "); 
-            System.Console.WriteLine("vilkden dag fyller du år i ? : ");
-            int dat1 = int.Parse(Console.ReadLine());
+                System.Console.WriteLine("skriv in månaden du är född i svara med 1 - 12 :");
+                var monthdate = int.Parse(Console.ReadLine());
+                string [] monthdayindex = {
+                    "januari",
+                    "februari",
+                    "mars",
+                    "april",
+                    "maj",
+                    "juni",
+                    "juli",
+                    "augusti",
+                    "september",
+                    "oktober",
+                    "november",
+                    "december"
+                };
 
-            string [] tec = {"stenbock", "vattunman" ,"Fiskarna", "väduren", "oxen", "tvillingen", "kräftan", "lejonet",
-            "ljungfrun", " vågen", "skorpionen", "skytten" 
-            };
-            int result = x && tec ;
+                int result = Array.IndexOf(monthdayindex, monthdate) +1;
+                System.Console.WriteLine(result);
 
-            for(int i = 0; i< dat; ++i)
+
+
+
+
+            int day = int.Parse(Console.ReadLine());
+            string stringday = "" + day;
+                if(stringday.lengt == 1){
+                   stringday = "0" + day;     
+                }
+
+                string completedate =  monthly + stringday;
+
+                int resultdata = int.Parse(completedate);
+
+                String starsign  = "";
+
+                if(321 >= && => 419 ) {
+                    starsign = "väduren";
+
+                }
+                else if(420 >= && => 520 ) {
+                    starsign = "oxen";
+
+                }
+                else if(521 >= && => 621 ) {
+                    starsign = "tvillingen";
+
+                }
+                else if(622 >= && => 722 ) {
+                    starsign = "kräftan";
+
+                }
+                else if(723 >= && => 822 ) {
+                    starsign = "lejonet";
+
+                }
+                else if(823 >= && => 922 ) {
+                    starsign = "ljungfrun";
+
+                }
+                else if(923 >= && => 1022 ) {
+                    starsign = "vågen";
+
+                }
+                else if(1023 >= && => 1121 ) {
+                    starsign = "skorpionen";
+
+                }
+                else if(1122 >= && => 119 ) {
+                    starsign = "skytten";
+
+                }
+                else if(120 >= && => 218 ) {
+                    starsign = "stenbocken";
+
+                }
+                else if(xxxx >= && => xxxx ) {
+                    starsign = "vattunmannen";
+
+                }
+                else if(219 >= && => 320 ) {
+                    starsign = "fiskar";
+
+                }
+
+                Console.WriteLine($"du är född i {month} och du är stjärntecknet {starsign}");
+        
             
+
+           
 
             }
             
